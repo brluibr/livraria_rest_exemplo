e39e95a1956625d413e70c8bc083cc8d1d980ff7
Bruno Luiz Brito <brunoluizbrito@hotmail.com>
2020-05-14 22:44:04 -0300

    Versão Inicial

---
+++ src/main/java/br/com/brluibr/livrariarest/LivrariarestApplication.java
@@ -0,0 +1,13 @@
+package br.com.brluibr.livrariarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brluibr/livrariarest/LivrariarestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brluibr.livrariarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
