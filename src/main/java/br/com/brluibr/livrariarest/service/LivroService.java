077d6fdf3ecf9a68489877ef39a23a5b780e8379
Bruno Luiz Brito <brunoluizbrito@hotmail.com>
2020-05-17 18:18:02 -0300

    Adicionado tratamento de exceção para livro não encontrado

--- src/main/java/br/com/brluibr/livrariarest/service/LivroService.java
+++ src/main/java/br/com/brluibr/livrariarest/service/LivroService.java
@@ -2,13 +2,12 @@ package br.com.brluibr.livrariarest.service;
 
 import br.com.brluibr.livrariarest.dto.LivroDTO;
 import br.com.brluibr.livrariarest.dto.MessagemRespostaDTO;
+import br.com.brluibr.livrariarest.exception.LivroNaoEncontradoException;
 import br.com.brluibr.livrariarest.mapper.LivroMapper;
 import br.com.brluibr.livrariarest.model.Livro;
 import br.com.brluibr.livrariarest.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class LivroService {
@@ -29,7 +28,8 @@ public class LivroService {
     }
 
     public LivroDTO buscarPorId (Long id) {
-        Optional<Livro> optionalLivro = livroRepository.findById(id);
-        return livroMapper.toDTO(optionalLivro.get());
+        Livro livro = livroRepository.findById(id)
+                .orElseThrow(() -> new LivroNaoEncontradoException(id));
+        return livroMapper.toDTO(livro);
     }
 }
